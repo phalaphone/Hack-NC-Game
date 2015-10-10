ca75f7647417edbffb68e6c2015d8be8cbbc74c5
akellyca <akellyca@live.unc.edu>
2015-10-10 14:36:03 -0400

    extend projectile

--- Bar.java
+++ Bar.java
@@ -1,4 +1,12 @@
 package Hack-NC-Game;
-public class Bar{
+public class Bar extends Projectile{
+
+    private int x_coord;
+    private int y_coord;
+    private int speed;
+    private int[] direction;
+    
+    public Bar(){
+    }
 	
 }
--- Projectile.java
+++ Projectile.java
@@ -1,4 +1,20 @@
 package Hack-NC-Game;
-public class Projectile{
 
+abstract public class Projectile{
+
+    private int x_coord;
+    private int y_coord;
+    private int[] direction;
+    private int speed;
+    
+    public Projectile(int avatar_x, int avatar_y, int mouse_x, int mouse_y){
+        this.x_coord = avatar_x;
+        this.y_coord = avatar_y;
+        Int[] direction = new Int[2];
+        direction[0] = mouse_x - avatar_x;
+        direction[1] = mouse_y - avatar_y;
+    }
+    
+    abstract public int getSpeed();
+    abstract public Projectile setSpeed();
 }
