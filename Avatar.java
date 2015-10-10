6f8535183dfb2bf7b7fe6fbf9acdb40be3891e4e
Funkativity <victor.murta97@gmail.com>
2015-10-10 17:04:54 -0400

    Completed update, draw, and loadContent

--- Avatar.java
+++ Avatar.java
@@ -1,24 +1,66 @@
 package Hack_NC_Game;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import java.util.Random;
+
 public class Avatar implements AvatarInterface{
 
 	private double xCoordinate, yCoordinate, width, height;
 	private final int frameWidth, frameHeight, 
 						xMin, xMax, yMin, yMax;
+	private final int speed = 1;
+	private BufferedImage avatarImage;
+	private final boolean isEnemy;
+	private final int color;
+	private BufferedImage spriteImg;
+	private final int spriteImgHeight,spriteImgWidth;
 	
-	Avatar(int x, int y, int frameWidth, int frameHeight){
+	private Avatar(int x, int y, int frameWidth, int frameHeight, boolean isEnemy){
 
 		xCoordinate = x;
 		yCoordinate = y;
 		this.frameWidth = frameWidth;
 		this.frameHeight = frameHeight;
-		width = frameHeight / 20;
-		height = frameHeight / 20;
-		xMin =  (int)(.5 * width);
-		xMax =  frameWidth + (int)(.5 * width);
-		yMin =  (int)(.5 * height);
-		yMax =  frameHeight + (int)(.5 * height);
+		width = frameHeight / 40;
+		height = frameHeight / 40;
+		xMin =  (int)(width);
+		xMax =  frameWidth + (int)(width);
+		yMin =  (int)(height);
+		yMax =  frameHeight + (int)(height);
+		this.isEnemy = isEnemy;
+		
+		Random rand;
+		if (isEnemy){
+			color = rand.nextInt(3) + 1;
+		}
+		else 
+			color = 0;
 	}
-
+	
+	public void LoadContent(){
+		
+        try
+        {
+            URL spriteImgUrl = this.getClass().getResource("/moon_lander/resources/images/sprite" 
+            													+ color + ".png");
+            spriteImg	 = ImageIO.read(spriteImgUrl);
+            spriteImgWidth = spriteImg.getWidth();
+            spriteImgHeight = spriteImg.getHeight();
+           
+        }
+        catch (IOException ex) {
+            Logger.getLogger(Avatar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}
 	public double getX(){
 		return xCoordinate;
 	}
@@ -41,7 +83,7 @@ public class Avatar implements AvatarInterface{
 		moveY(yIncrement);
 	}
 	
-	private void moveX(int xIncrement){
+	private void moveX(double xIncrement){
 		
 		if (xMin <= xCoordinate + xIncrement  &&
 				xMax >= xCoordinate + xIncrement){
@@ -59,7 +101,7 @@ public class Avatar implements AvatarInterface{
 		}
 	}
 	
-	private void moveY(int yIncrement){
+	private void moveY(double yIncrement){
 		if (yMin <= yCoordinate + yIncrement  &&
 				yMax >= yCoordinate + yIncrement){
 			
@@ -75,4 +117,27 @@ public class Avatar implements AvatarInterface{
 			return;
 		}	
 	}
+	
+	public void Update() {
+	        
+	        if(Canvas.keyboardKeyState(KeyEvent.VK_W))
+	            moveY(speed);
+	        
+	        if(Canvas.keyboardKeyState(KeyEvent.VK_A))
+	        	moveX(-speed);
+	        
+	        if(Canvas.keyboardKeyState(KeyEvent.VK_S))
+	        	moveY(-speed);
+	        
+	        if(Canvas.keyboardKeyState(KeyEvent.VK_D))
+	        	moveX(speed);
+	    }
+	    
+	    public void Draw(Graphics2D g2d)
+	    {
+	        g2d.setColor(Color.white);
+	        g2d.drawImage(spriteImg,(int) xCoordinate, (int) yCoordinate, null);
+
+	    }
+	    	
 }	
--- AvatarInterface.java
+++ AvatarInterface.java
@@ -1,10 +1,13 @@
 package Hack_NC_Game;
 public interface AvatarInterface{
 
-	//Avatar(int x, int y, int frameWidth, int frameHeight);
+	//Avatar(int x, int y, int frameWidth, int frameHeight, boolean isEnemy);
 	double getX();
 	double getY();
 	double getWidth();
 	double getHeight();
 	void move(int xIncrement, int yIncrement);
+	void Update();  //moves itself if user is pressing WASD
+	void Draw();    //draws itself to the canvas
+	void LoadContent(); //loads images, to be run after construction 
 }
