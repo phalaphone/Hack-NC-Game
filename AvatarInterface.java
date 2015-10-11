9edb04c724b9acb87824e47cca16fce3d8fc0dcf
AMichaelK <akan@live.unc.edu>
2015-10-10 20:01:58 -0400

    Fixed void Draw() AvatarInterface

--- AvatarInterface.java
+++ AvatarInterface.java
@@ -1,4 +1,7 @@
 package Hack_NC_Game;
+
+import java.awt.Graphics2D;
+
 public interface AvatarInterface{
 
 	//Avatar(int x, int y, int frameWidth, int frameHeight, boolean isEnemy);
@@ -8,6 +11,6 @@ public interface AvatarInterface{
 	double getHeight();
 	void move(int xIncrement, int yIncrement);
 	void Update();  //moves itself if user is pressing WASD
-	void Draw();    //draws itself to the canvas
+	void Draw(Graphics2D g2d);    //draws itself to the canvas
 	void LoadContent(); //loads images, to be run after construction 
 }
